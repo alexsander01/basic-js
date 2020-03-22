e985fca5250b4578f31bbc5705ad3ba224d86ca0
Alexsander Brantsevich <lexsander.red.34@gmail.com>
2020-03-22 23:51:25 +0300

    Task done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,91 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(type) {
+        this.type = true;
+        if (type == false) this.type = false;
+    }
+    encrypt(message, key) {
+        if (typeof message !== 'string' || typeof key !== 'string') {
+            throw new UserError('wrong arguments');
+        }
+
+        let messagearr = message.toUpperCase().split('');
+        let keyword = key.toUpperCase();
+        let abc = '';
+        for (let i = 65; i <= 90; i++) {
+            abc += String.fromCodePoint(i);
+        }
+        let ABC = abc.split('');
+
+        while (keyword.length < message.length) {
+            keyword += keyword;
+        }
+        keyword = keyword.slice(0, message.length).split('');
+
+        let res = [];
+        for (let i = 0, j = 0; i < messagearr.length; i++) {
+            if (/[A-Z]/.test(messagearr[i])) {
+                //Ci=(Mi+Ki)%26
+                let MIndex = ABC.indexOf(messagearr[i]);
+                let KIndex = ABC.indexOf(keyword[j]);
+
+                let a = MIndex + KIndex;
+                let CIndex = a % 26;
+                res.push(ABC[CIndex]);
+                j++;
+            }
+            else {
+                res.push(messagearr[i]);
+            }
+        }
+
+        if (!this.type) {
+            return res.reverse().join("");
+        }
+        else return res.join("");
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+        if (typeof encryptedMessage !== 'string' || typeof key !== 'string') {
+            throw new UserError('wrong arguments');
+        }
+        let encryptedMessagearr = encryptedMessage.toUpperCase().split('');
+        let keyword = key.toUpperCase();
+        let abc = '';
+        for (let i = 65; i <= 90; i++) {
+            abc += String.fromCodePoint(i);
+        }
+        let ABC = abc.split('');
+
+        while (keyword.length < encryptedMessage.length) {
+            keyword += keyword;
+        }
+        keyword = keyword.slice(0, encryptedMessage.length).split('');
+
+        let res = [];
+        for (let i = 0, j = 0; i < encryptedMessagearr.length; i++) {
+            if (/[A-Z]/.test(encryptedMessagearr[i])) {
+                //Mi=(Ci-Ki)%26
+                let CIndex = ABC.indexOf(encryptedMessagearr[i]);
+                let KIndex = ABC.indexOf(keyword[j]);
+
+                if (CIndex < KIndex) {
+                    let MIndex = 26 + (CIndex - KIndex);
+                    res.push(ABC[MIndex]);
+                }
+                else {
+                    let MIndex = (CIndex - KIndex) % 26;
+                    res.push(ABC[MIndex]);
+                }
+                j++;
+
+            }
+            else res.push(encryptedMessagearr[i]);
+        }
+        if (!this.type) {
+            return res.reverse().join("");
+        }
+        else return res.join("");
     }
+
 }
-
 module.exports = VigenereCipheringMachine;
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
+
+module.exports = function getSeason(date ) {
+  
   // remove line with error and write your code here
+  if(!date){return "Unable to determine the time of year!";}
+	else if(Date.prototype.toString.call(date) === "Invalid Date"){throw "Invalid date"; return;}
+	
+	if(/Sep|Oct|Nov/.test(date) === true){return "autumn";}
+	else if(/Dec|Jan|Feb/.test(date) === true){return "winter";}
+	else if(/Mar|Apr|May/.test(date) === true){return "spring";}
+	else if(/Jun|Jul|Aug/.test(date) === true){return "summer";}
+	return;
 };
